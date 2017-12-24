95d2d38670b9cec16191e3e8d07dcfbb47aaa7cb
suman <suman@tardins.com>
2017-12-25 01:09:33 +0530

    Add Code comments
    Added Java doc comments
    
    Change-Id: I4de95e94d0397cfd6801c38f15701cdc0777d5e9

--- src/com/tarini/cache/impl/CFactory.java
+++ src/com/tarini/cache/impl/CFactory.java
@@ -1,12 +1,41 @@
+/***************************************************************************************
+ * This software is Copyright (C) 2017 by Suman Mummaneni 
+ * This software is licensed under LGPL License agreement 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ ***************************************************************************************/
 package com.tarini.cache.impl;
 
+/**
+ * A factory for creating C objects.
+ */
 public class CFactory implements CacheFactory {
 
+    /** The instance. */
     private static CFactory instance;
     
+    /**
+     * Instantiates a new c factory.
+     */
     private CFactory() {
     }
     
+    /**
+     * Gets the single instance of CFactory.
+     *
+     * @return single instance of CFactory
+     */
     public static CFactory getInstance() {
         if(instance == null) {
             synchronized(CFactory.class) {
@@ -17,6 +46,9 @@ public class CFactory implements CacheFactory {
     }
     
     
+    /* (non-Javadoc)
+     * @see com.tarini.cache.impl.CacheFactory#getLRUCache(java.lang.Class)
+     */
     @Override
     public Cache<?> getLRUCache(Class<?> t) {
         //TODO: Fix this code needs to look at it.
